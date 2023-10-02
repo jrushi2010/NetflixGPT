4db83cc378f846bd1f7e639d731b803e0f7ea907
“Rushikesh <jrushi2010@gmail.com>
2023-10-02 14:28:32 +0530

    added movieSlice and store the movie data to the appStore

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import Header from './Header';
 import {API_OPTIONS} from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import {addNowPlayingMovies} from '../utils/moviesSlice';
 
 const Browse = () => {
+
+  const dispatch = useDispatch();
 
   const getNowPlayingMovies = async () =>{
     const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);
@@ -10,6 +14,8 @@ const Browse = () => {
     const json = await data.json();
 
     console.log(json.results);
+
+    dispatch(addNowPlayingMovies(json.results));
   
   }
 
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from '../utils/userSlice';
+import moviesSlice from '../utils/moviesSlice';
 
 const appStore = configureStore({
     reducer : {
         user: userSlice,
+        movies: moviesSlice,
     },
 })
 
