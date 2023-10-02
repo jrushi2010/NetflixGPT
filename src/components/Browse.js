cd2030924b5b093ff12fe2fade81f80a4b4c3009
“Rushikesh <jrushi2010@gmail.com>
2023-10-02 14:18:56 +0530

    added tmdb fetch api

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,22 @@
-import React from 'react'
-import Header from './Header'
+import React, { useEffect } from 'react';
+import Header from './Header';
+import {API_OPTIONS} from '../utils/constants';
 
 const Browse = () => {
+
+  const getNowPlayingMovies = async () =>{
+    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);
+  
+    const json = await data.json();
+
+    console.log(json.results);
+  
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  },[]);
+
   return (
     <div>
       <Header/>  
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const userIcon = "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABY20DrC9-11ewwAs6nfEgb1vrORxRPP9IGmlW1WtKuaLIz8VxCx5NryzDK3_ez064IsBGdXjVUT59G5IRuFdqZlCJCneepU.png?r=229";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmM4ZGFhN2YzNmQ5NDUzYzJmYTUyMWIzNTFmMDYxZSIsInN1YiI6IjY1MWE2NTNjOTNiZDY5MDBlMTJlYTk2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Nt6l5jITfN8NwZHiQeCIJywChC3jD8Dlo5WyoS8WBSo'
+    }
+  };
