03e0a70a9ca968bd270833557f48661923ffd34e
“Rushikesh <jrushi2010@gmail.com>
2023-10-02 11:32:10 +0530

    hard coded things moved to the constant file

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const userIcon = "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABY20DrC9-11ewwAs6nfEgb1vrORxRPP9IGmlW1WtKuaLIz8VxCx5NryzDK3_ez064IsBGdXjVUT59G5IRuFdqZlCJCneepU.png?r=229";
