cbf2c4a8198c7e561c033ffbad49c6aa6cb94de9
“Rushikesh <jrushi2010@gmail.com>
2023-10-03 15:44:55 +0530

    removed scroll bar which was visible

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,7 +6,7 @@ const MovieList = ({ title, movies }) => {
   return (
     <div className='px-6'>
        <h1 className='text-3xl py-4 text-white'>{title}</h1>
-      <div className='flex overflow-x-scroll'>
+      <div className='flex hover:overflow-x-scroll'>
         <div className='flex'>
           {movies?.map (movie => <MovieCard key={movie.id} posterPath={movie.poster_path}/>)}
         </div>
